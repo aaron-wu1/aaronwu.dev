5fafd94d295c210eeb192fa40b24822bf478079f
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2023-07-14 19:03:01 -0700

    removed emojis, added icons

--- src/about.tsx
+++ src/about.tsx
@@ -26,31 +26,120 @@ function About() {
 
       <div className="flex flex-row space-x-4 h-screen w-screen justify-evenly snap-y snap-mandatory snap-always overflow-scroll">
         <div className="relative top-1/4 flex flex-col space-y-4">
-          <p className="text-5xl font-bold text-left pb-2 ">🧑🏻‍💻 Aaron Wu</p>
-          <div className="flex flex-row gap-x-4 max-w-sm text-l text-left text-slate-400">
-            <div>🔭</div>
+          <p className="text-5xl font-bold text-left pb-2 ">Aaron Wu</p>
+          <div className="flex flex-row gap-x-5 max-w-sm text-l text-left text-slate-400">
+            <div className="self-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-zap"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+              </svg>{" "}
+            </div>
             <div>
               Exploring new technologies and engineering to help others!
             </div>
           </div>
-          <div className="flex flex-row gap-x-4 max-w-sm text-l text-left text-slate-400">
-            <div>🎓</div>
+          <div className="flex flex-row gap-x-5 max-w-sm text-l text-left text-slate-400">
+            <div className="self-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-database"
+              >
+                <ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
+                <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+                <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+              </svg>
+            </div>
             <div>
               Studying Computer Science at University of California, Santa Cruz.
             </div>
           </div>
-          <div className="flex flex-row gap-x-4 max-w-sm text-l text-left text-slate-400">
-            <div>💼</div>
+          <div className="flex flex-row gap-x-5 max-w-sm text-l text-left text-slate-400">
+            <div className="self-center">
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-briefcase"
+              >
+                <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+                <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+              </svg>
+            </div>
             <div>Working as a Full Stack Developer at Jlab smart sensing.</div>
           </div>
-          <div className="flex flex-row gap-x-4 max-w-sm text-l text-left text-slate-400">
-            <div>🌱</div>
+          <div className="flex flex-row gap-x-5 max-w-sm text-l text-left text-slate-400">
+            <div className="self-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-cpu"
+              >
+                <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
+                <rect x="9" y="9" width="6" height="6"></rect>
+                <line x1="9" y1="1" x2="9" y2="4"></line>
+                <line x1="15" y1="1" x2="15" y2="4"></line>
+                <line x1="9" y1="20" x2="9" y2="23"></line>
+                <line x1="15" y1="20" x2="15" y2="23"></line>
+                <line x1="20" y1="9" x2="23" y2="9"></line>
+                <line x1="20" y1="14" x2="23" y2="14"></line>
+                <line x1="1" y1="9" x2="4" y2="9"></line>
+                <line x1="1" y1="14" x2="4" y2="14"></line>
+              </svg>
+            </div>
             <div>
               Learning more about Distributed Systesms and Systems Design.
             </div>
           </div>
-          <div className="flex flex-row gap-x-4 max-w-sm text-l text-left text-slate-400">
-            <div>👯</div>
+          <div className="flex flex-row gap-x-5 max-w-sm text-l text-left text-slate-400">
+            <div className="self-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-compass"
+              >
+                <circle cx="12" cy="12" r="10"></circle>
+                <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+              </svg>
+            </div>
             <div>
               Currently I'm looking for a software enginering internship or a
               new grad role
