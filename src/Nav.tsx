cbf1a18bf7d238e6c5674b2272e42c549145f79d
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-02-29 05:11:56 -0800

    updated links

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,18 +1,12 @@
+import { data } from './data';
 function Nav() {
+  const links = data.links;
   return (
     <div className='absolute flex flex-row gap-5 p-5 z-50 text-right right-5  text-slate-400'>
-      <a
-        className='hover:text-slate-200'
-        href='https://drive.google.com/file/d/1HHmsudfMJKng_To7TAgHS5jekYZjRWXC/view?usp=sharing'
-        target='_blank'
-      >
+      <a className='hover:text-slate-200' href={links.resume} target='_blank'>
         Resume
       </a>
-      <a
-        className='hover:text-slate-200'
-        href='https://github.com/Aaron-Wu1'
-        target='_blank'
-      >
+      <a className='hover:text-slate-200' href={links.github} target='_blank'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           width='24'
@@ -27,11 +21,7 @@ function Nav() {
           <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
         </svg>
       </a>
-      <a
-        className='hover:text-slate-200'
-        href='https://www.linkedin.com/in/aaron-wu1/'
-        target='_blank'
-      >
+      <a className='hover:text-slate-200' href={links.linkedin} target='_blank'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           width='24'
